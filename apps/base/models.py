5c9adc2f409077668ec108865051bf740d397377
juan <juankrios15@gmail.com>
2024-04-09 17:53:11 -0500

    soft skill logo

--- apps/base/models.py
+++ apps/base/models.py
@@ -29,7 +29,7 @@ class SoftSkill(models.Model):
     monster_name = models.CharField(max_length=100, blank=True, null=True)
     monster_profile = models.TextField(blank=True, null=True)
     monster_picture = models.ImageField(upload_to="monsters_pics/", null=True, blank=True)
-
+    logo = models.FileField(upload_to="logos/", null=True, blank=True)
 
     def __str__(self):
         return self.name
