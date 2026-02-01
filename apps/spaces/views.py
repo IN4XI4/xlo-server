4d76249b9b610df1c95eaa20762414da66b4d76d
Juan <juankrios15@gmail.com>
2026-01-31 22:26:06 -0500

    added: improvements in views

--- apps/spaces/views.py
+++ apps/spaces/views.py
@@ -28,7 +28,7 @@ from apps.users.serializers import UserDetailSerializer
 
 
 class SpacesPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 15
     page_size_query_param = "page_size"
     max_page_size = 50
 
